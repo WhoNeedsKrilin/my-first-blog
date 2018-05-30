b53e191922171169a874ae642e10fdd077fc076f
Laurence <gabrielrebadulla@gmail.com>
2018-05-31 01:13:36 +0800

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
-        default=timezone.now
+        default=timezone.now    
     )
 
     published_date = models.DateField(
